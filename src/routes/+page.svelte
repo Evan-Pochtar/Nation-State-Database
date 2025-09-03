1b1f693bc3231c9580395b42e28fa3c71baceb3a
Evan Pochtar <pocht004@umn.edu>
2025-09-03 15:00:11 -0500

    Using Prettier & Adding Area for API Calls

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,12 +1,17 @@
 <script lang="ts">
-  import Map from '$lib/Map.svelte';
+	import Map from '$lib/Map.svelte';
 </script>
 
-<style global>
-  :root { color-scheme: light; }
-  :global(body) { background: #000; color: #fff; }
-</style>
-
 <main class="h-screen w-screen">
-  <Map />
+	<Map />
 </main>
+
+<style global>
+	:root {
+		color-scheme: light;
+	}
+	:global(body) {
+		background: #000;
+		color: #fff;
+	}
+</style>
